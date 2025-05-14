510cfbc96d22c5c92e886ae981ff094bbe59dc8a
GiuliaCeretto <s339483@studenti.polito.it>
2025-05-14 11:57:31 +0200

    Parvenza di menu

---
+++ Assets/Scripts/Menu/LivelliMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LivelliMenu : MonoBehaviour
+{
+    public void PlayLivelloUno()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    public void PlayLivelloDue()
+    {
+        //SceneManager.LoadSceneAsync(3);
+    }
+    public void PlayLivelloTre()
+    {
+        //SceneManager.LoadSceneAsync(4);
+    }
+    public void PlayLivelloQuattro()
+    {
+        //SceneManager.LoadSceneAsync(5);
+    }
+}
