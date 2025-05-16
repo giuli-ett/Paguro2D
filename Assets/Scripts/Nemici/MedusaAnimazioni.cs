cfde53e96e81c57319896c1552c1450cb2077f18
lelee-01 <s337803@studenti.polito.it>
2025-05-16 18:58:30 +0200

    finito scena trailer

--- Assets/Scripts/Nemici/MedusaAnimazioni.cs
+++ Assets/Scripts/Nemici/MedusaAnimazioni.cs
@@ -8,32 +8,15 @@ public class MedusaAnimazioni : MonoBehaviour
     private void Start()
     {
         animator = GetComponent<Animator>();
-        animator.SetInteger("stato", 0); // Inizia in Idle
+        animator.SetBool("playerOnTop", false); // Inizia in Idle
     }
 
-    private void Update()
-    {
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-
-        if (isPlayerOnTop)
-        {
-            animator.SetInteger("stato", 1); // Passa a Schiacciata
-        }
-        else if (stateInfo.IsName("Schiacciata"))
-        {
-            animator.SetInteger("stato", 2); // Passa a Rimbalzo
-        }
-        else if (stateInfo.IsName("Rimbalzo") && stateInfo.normalizedTime >= 1f)
-        {
-            animator.SetInteger("stato", 0); // Torna a Idle quando rimbalzo finisce
-        }
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
-            isPlayerOnTop = true;
+           animator.SetBool("playerOnTop", true);
         }
     }
 
@@ -41,7 +24,7 @@ public class MedusaAnimazioni : MonoBehaviour
     {
         if (collision.collider.CompareTag("Player"))
         {
-            isPlayerOnTop = false;
+            animator.SetBool("playerOnTop", false);
         }
     }
 }
