cdc9dd20ea40eca4206e852e4f69c809393855dd
lelee-01 <s337803@studenti.polito.it>
2025-05-30 01:26:47 +0200

    meta primo livello fatto

--- Assets/Scripts/Nemici/Pesci.cs
+++ Assets/Scripts/Nemici/Pesci.cs
@@ -8,16 +8,34 @@ public class Pesci : MonoBehaviour
     [Header("VARIABILI")]
     [SerializeField] private float velocita = 2f;
     [SerializeField] private float distanza = 1.2f;
+    private SpriteRenderer spriteRenderer;
+    private float lastX;
 
     private void Start()
     {
         startPosition = transform.position;
+        lastX = startPosition.x;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Move()
     {
         float newX = startPosition.x + Mathf.Sin(Time.time * velocita) * distanza;
         transform.position = new Vector3(newX, startPosition.y, startPosition.z);
+
+        
+        float deltaX = newX - lastX;
+
+        if (deltaX > 0.01f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (deltaX < -0.01f)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        lastX = newX;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
