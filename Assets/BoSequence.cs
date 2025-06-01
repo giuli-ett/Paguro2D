c9561844376b1178e914044b9a220c8a70be17b4
giuli-ett <guliaavola@gmail.com>
2025-06-01 16:58:35 +0200

    prova

---
+++ Assets/BoSequence.cs
@@ -0,0 +1,26 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Composite = Unity.Behavior.Composite;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "bo", category: "Flow", id: "b968e4e67791c089c4d8d5f966768287")]
+public partial class BoSequence : Composite
+{
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
