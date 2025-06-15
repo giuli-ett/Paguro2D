314ba35eca72cd0190f9fef00234b2f781821edd
GiuliaCeretto <s339483@studenti.polito.it>
2025-06-15 22:47:07 +0200

    Interfaccia MainMenu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -6,5 +6,16 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+        // DA METTERE LA CUTSCENE INIZIALE
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
