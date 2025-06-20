4e082e583e3af73eb2a431b7e37680b55b5870a5
giuli-ett <guliaavola@gmail.com>
2025-06-20 21:50:13 +0200

    audio

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -27,7 +27,6 @@ public class AudioManager : MonoBehaviour
     public AudioClip navigateInventory;
     public AudioClip checkPoint;
     public AudioClip collezionabile;
-    public AudioClip vittoria;
 
     void Awake()
     {
@@ -129,10 +128,5 @@ public class AudioManager : MonoBehaviour
     {
         audioSource.loop = false;
         audioSource.PlayOneShot(bubbles);
-    }
-    public void PlayVittoria()
-    {
-        audioSource.loop = false;
-        audioSource.PlayOneShot(vittoria);
     }
 }
