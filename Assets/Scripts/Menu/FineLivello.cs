5ad1ac836c3dcd7e0438a788af841c3747b2f1e9
GiuliaCeretto <s339483@studenti.polito.it>
2025-06-20 10:01:39 +0200

    Animazione fine livello

--- Assets/Scripts/Menu/FineLivello.cs
+++ Assets/Scripts/Menu/FineLivello.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,8 +8,28 @@ public class FineLivello : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+
+            // Imposta il livello come completato
             GameManager.Instance.currentLivello.isCompleted = true;
-            SceneManager.LoadSceneAsync(5);
+
+            // Avvia la coroutine per l'animazione e il cambio scena
+            StartCoroutine(FineLivelloCoroutine(other.gameObject));
         }
+    }
+
+    private IEnumerator FineLivelloCoroutine(GameObject player)
+    {
+        // Attiva l'animazione di vittoria
+        Animator anim = player.GetComponent<Animator>();
+        if (anim != null)
+        {
+            anim.SetBool("isWinning", true);
+        }
+
+        // Aspetta 1 secondo
+        yield return new WaitForSeconds(0.8f);
+
+        // Cambia scena
+        SceneManager.LoadSceneAsync(1);
     }
 }
