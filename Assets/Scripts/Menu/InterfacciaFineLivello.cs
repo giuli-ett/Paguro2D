46a305febf30818bcc4b9465a54f920f5df07cd9
giuli-ett <guliaavola@gmail.com>
2025-09-07 18:06:18 +0200

    fine livello controller

--- Assets/Scripts/Menu/InterfacciaFineLivello.cs
+++ Assets/Scripts/Menu/InterfacciaFineLivello.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InterfacciaFineLivello : MonoBehaviour
 {
@@ -9,6 +12,13 @@ public class InterfacciaFineLivello : MonoBehaviour
     public TextMeshProUGUI number;
     public GameObject livello1;
     public GameObject livello2;
+    [Header("UI")]
+    public Button[] menuButtons;
+    public Color32 originalColor;
+    public Color32 color = new Color32(176, 159, 173, 255);
+    private bool canNavigate = true;
+    public float stickThreshold = 0.5f;
+    private int currentIndex = -1;
 
     private void Awake()
     {
@@ -18,14 +28,19 @@ public class InterfacciaFineLivello : MonoBehaviour
             return;
         }
         Instance = this;
+
+        if (menuButtons.Length > 0)
+        {
+            originalColor = menuButtons[0].GetComponent<Image>().color;
+        }
     }
-    
+
     private void OnEnable()
     {
         if (GameManager.Instance != null)
             GameManager.Instance.ItemCollectedEvent += OnItemCollected;
     }
-    
+
     private void OnDisable()
     {
         if (GameManager.Instance != null)
@@ -53,7 +68,7 @@ public class InterfacciaFineLivello : MonoBehaviour
             Debug.Log("Torno alla home");
             SceneManager.LoadSceneAsync(0);
         }
-        
+
     }
 
     public void TornaMenu()
@@ -79,11 +94,90 @@ public class InterfacciaFineLivello : MonoBehaviour
             livello1.SetActive(false);
             livello2.SetActive(true);
         }
-        
-        
+
+
         if (number != null && GameManager.Instance != null)
         {
             number.text = $"Collezionabili: {GameManager.Instance.TotalCollected}/3";
         }
+    }
+    
+    void Update()
+    {
+        if (GameManager.Instance.isUsingController)
+        {
+            Vector2 nav = Vector2.zero;
+            if (Gamepad.current != null)
+            {
+                nav = Gamepad.current.leftStick.ReadValue();
+            }
+
+            if (canNavigate)
+            {
+                if (nav.y > stickThreshold)
+                {
+                    MoveSelection(-1);
+                    canNavigate = false;
+                }
+                else if (nav.y < -stickThreshold)
+                {
+                    MoveSelection(1);
+                    canNavigate = false;
+                }
+            }
+            if (Mathf.Abs(nav.y) < stickThreshold)
+            {
+                canNavigate = true;
+            }
+
+            if (Gamepad.current != null && Gamepad.current.buttonSouth.wasPressedThisFrame)
+            {
+                PressCurrentButton();
+            }
+        }
+        else
+        {
+            if (Keyboard.current.enterKey.wasPressedThisFrame)
+            {
+                PressCurrentButton();
+            }
+        }
+    }
+
+    private void MoveSelection(int direction)
+    {
+        if (menuButtons.Length == 0) return;
+
+        DeselectAllSlots();
+
+        currentIndex += direction;
+        if (currentIndex < 0) currentIndex = menuButtons.Length - 1;
+        if (currentIndex >= menuButtons.Length) currentIndex = 0;
+
+        HighlightSlot(currentIndex);
+        EventSystem.current.SetSelectedGameObject(menuButtons[currentIndex].gameObject);
+    }
+
+    private void PressCurrentButton()
+    {
+        if (menuButtons.Length == 0) return;
+
+        var button = menuButtons[currentIndex];
+        if (button != null)
+        {
+            button.onClick.Invoke();
+        }
+    }
+
+    private void HighlightSlot(int index)
+    {
+        if (menuButtons.Length == 0) return;
+        menuButtons[index].GetComponent<Image>().color = color;
+    }
+
+    public void DeselectAllSlots()
+    {
+        foreach (var slot in menuButtons)
+            slot.GetComponent<Image>().color = originalColor;
     }
 }
